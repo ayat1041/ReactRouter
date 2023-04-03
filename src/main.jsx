0b6f32415f68a789345e613d8449389115f2e3ff
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-04-03 14:58:25 +0600

    updated href to link

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
-  {
-    path: "/home",
-    element: <div><h1>Hello home!</h1></div>,
-  },
+  // {
+  //   path: "/contacts/2",
+  //   element: <div><h1>Hello home!</h1></div>,
+  // },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
